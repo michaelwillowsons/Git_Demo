dde96deaba1678ede0f88671756e87db0e385b3a
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-23 13:51:20 +0100

    added gitignore

--- adding.py
+++ adding.py
@@ -5,8 +5,9 @@ Created on Thu May 23 12:46:29 2024
 @author: willom
 """
 # function to add two numbers
-def add(a,b):
-    ans = a+b
+# Default values: 1,2,0
+def add(a=1,b=2, c=0):
+    ans = a+b +c
     return ans
 
 print(add(3,4))
