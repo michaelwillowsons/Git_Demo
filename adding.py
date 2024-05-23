9298d174460b32b6ae3b8a48d89f43b58a32fd43
Dunne-Willows <Michael.Willows@ons.gov.uk>
2024-05-23 13:33:01 +0100

    Update adding.py
    Added documentation
    Added test case

--- adding.py
+++ adding.py
@@ -4,9 +4,15 @@ Created on Thu May 23 12:46:29 2024
 
 @author: willom
 """
-
+# function to add two numbers
 def add(a,b):
     ans = a+b
     return ans
 
 print(add(3,4))
+
+# test case
+# 5+6=11
+
+if add(5,6) ==11:
+    print("Test passed")
