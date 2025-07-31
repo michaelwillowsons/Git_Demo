1b73cfb4e811ef4415b666fa7325b1e27ae69147
michaelwillowsons <119348069+michaelwillowsons@users.noreply.github.com>
2025-07-31 13:18:43 +0100

    Update Adder.py

--- Adder.py
+++ Adder.py
@@ -5,8 +5,8 @@ Created on Thu Jul 31 13:14:07 2025
 @author: willom
 """
 
-def add(a,b):
-    ans = a + b
+def add(a,b,c):
+    ans = a + b + c
     return ans
 
-print(add(3,4) == 7)
+print(add(3,4, 0) == 7)
