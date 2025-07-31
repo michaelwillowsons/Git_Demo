33d16670d3126e262a42638d1856d09b8750a81c
Dunne-Willows <Michael.Willows@ons.gov.uk>
2025-07-31 15:37:36 +0100

    Update adder.py
    Added a third variable

--- adder.py
+++ adder.py
@@ -5,8 +5,8 @@ Created on Thu Jul 31 13:21:13 2025
 @author: willom
 """
 
-def adder(a,b):
-    ans = a+b 
+def adder(a,b,c):
+    ans = a+b+c 
     return ans
 
-print(adder(3,4)==7)
+print(adder(3,4,0)==7)
